9e7757e17b7826c097e5934c4e1357af96889157
Bradley Huchteman <sypsyn@gmail.com>
2020-01-06 10:11:58 -0500

    favicon bug and other cleanup

--- src/_data/instagram.js
+++ src/_data/instagram.js
@@ -1,17 +1,17 @@
-const axios = require('axios');
-const fs = require('fs');
+// const axios = require('axios');
+// const fs = require('fs');
 
-require('dotenv').config();
-const INSTAGRAM_AUTH = process.env.INSTAGRAM_AUTH;
-var url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${INSTAGRAM_AUTH}`;
+// require('dotenv').config();
+// const INSTAGRAM_AUTH = process.env.INSTAGRAM_AUTH;
+// var url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${INSTAGRAM_AUTH}`;
 
-module.exports = async function() {
-  return axios
-    .get(url)
-    .then(function(response) {
-      return response.data.data;
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-};
+// module.exports = async function() {
+//   return axios
+//     .get(url)
+//     .then(function(response) {
+//       return response.data.data;
+//     })
+//     .catch(function(error) {
+//       console.log(error);
+//     });
+// };
